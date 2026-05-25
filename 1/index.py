f6e930184c2791a342b5424d97c29ca3bfa82ef2
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2026-05-25 20:30:32 +0500

    variables

---
+++ 1/index.py
@@ -0,0 +1,33 @@
+# Variables - o'zgaruvchilar
+# Python doesn't have any command for declaring a variable. - Pythonda o'zgaruvchi elon qilishdan oldin hech nima yozilmaydi
+
+
+# Python name the variables are the same as JS - Pythonda o'zgaruvchi nomlash JS bn bir xil
+
+name = 'Roziya'
+age = 17
+print(name) # print function for output the values - print funksiyasi qiymatni ekranga chiqarish uchun
+
+# You can assign values to multiple variables in one line - bitta qatorda koplab qiymatlarni koplab o'zgaruvchilarga o'zlashtirsa bo'ladi
+
+fun, smart, essential = "sudoku", "chess", "badminton" 
+
+# You can assign same value to multiple variables - bitta qiymatni bir nechta o'zgaruvchiga o'zlashtirsak bo'ladi
+
+anime = family = spy = 'Spy X Family' 
+
+# All math things are same and you can add str to number - hamma matematik amallar bir xil va str ni numga qosholmesiz
+
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 + num_2) # 5
+print(num_1 - num_2) # 1
+print(num_1 * num_2) # 6
+print(num_1 / num_2) # 1.5
+print(num_1 % num_2) # 1 
+print(num_1 ** num_2) # 9
+
+
+# They also have global variable and local variable as JS - JS dek bundayam  global va local o'zgaruvchi degan narsalar bor
