916ba70bb4a52c4829b8fa68539c9599e602be0d
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2026-05-28 20:22:34 +0500

    learning set data type

--- 1/data_types.py
+++ 1/data_types.py
@@ -47,8 +47,31 @@ future_goal = {"personality":"curios", "career":"cosmonovt", "home":"Saudia"}
 
 # key must be immutable but we do not care about value - kalit so'z o'zgarmas bo'lishi kerak qiymatlarini farqi yo'q
 # I mean list is mutable - Aytmoqchi bo'lganim list o'zgaruvchan shuning uchun key bo'la olmaydi
-
+# no duplicate is allowed
+# ordered and changable 
 # smth = {[1,2]:"bir ikki"} # error beradi
 
 future_goal['personality'] #we need to call value by key like this not like future_goal.personality
 
+# ----------- Set Types -----------
+# set - no duplications, if you add smth twice it will appear only once
+# the items are unchangable but you can add and remove it
+# the items will be printed unordered like it will show randomly
+
+# the value False and 0  or smth like that treated like a duplicate so one of them will be removed when it was published
+# len(set_name) - is equal to set_name.length in JS
+# adding 2 sets you need to do 
+
+thisset = {"Harry", 'Draco'}
+newOne = {"Hermione", 'Ron'}
+
+thisset.update(newOne) # merging 2 sets
+thisset.add("Cedric") # adding to set
+thisset.remove("Ron") # if you delete item that doesn't exist then it will raise an error
+thisset.remove("Hermione") # if you delete item that doesn't exist then it will NOT raise an error
+
+thisset.pop() # it will remove random item
+thisset.clear() # it will clear all items from the set
+del thisset # it will remove entire set
+
+print(thisset)
