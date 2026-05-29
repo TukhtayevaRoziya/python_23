dbb95c557a850b053c10ffda76aee5248d963f88
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2026-05-29 14:38:54 +0500

    relearning data types one by one

---
+++ 1/number.py
@@ -0,0 +1,16 @@
+# we have 3
+# int
+# float
+# complex
+
+age = 17
+real_age =17.75
+idk = 17j
+
+# smth = float(age) # converting int to float
+# smth = int(real_age) # converting float to int it won't yaxlitlaydi it just get rid of extra numbers after point
+smth = complex(age) # (17+0j)
+
+# Note you cannot convert from COMPLEX to smth else
+
+print(smth)
