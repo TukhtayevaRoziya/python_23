4fa6b628ec70df7d3101238a8aa57943f856af1f
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2026-06-01 21:15:12 +0500

    string datatype

---
+++ 1/string.py
@@ -0,0 +1,38 @@
+name = """
+Roziya
+"""
+
+# if you wanna type multiple lines you need to put triple(3x) quote
+
+# in - its like includes as js string
+
+
+# your_word in that_string
+
+print("Roziya" in name) # True
+
+# not in - opposite of it
+
+print("Roziya" not in name) # False
+
+# [start_index: end_index] - its like slice in JS
+
+b = "Hello, World!"
+print(b[2:5]) # llo
+
+
+# Note: if you dont put start index or end index it will automaticly put 0 and end of the string index
+
+# Note: negative number same here but it will start counting from end
+
+
+# .upper()
+# .lower()
+# .strip() - it is like trim in JS
+
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+# f-string will allow you to use variables or any outside thing in the string inside
