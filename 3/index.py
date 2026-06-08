d47f9fe70e3f733c7453d5da0ca2b369e4d07821
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2026-06-08 21:26:08 +0500

    second video

---
+++ 3/index.py
@@ -0,0 +1,20 @@
+name= "John"
+age = 20
+is_new = True
+
+name = input("WHat is your name? ")
+color = input("Which color do you like? ")
+
+print(f"{name} likes {color}")
+
+
+pounds = input("What is you weight in pounds? ")
+kg = 0.453592 * int(pounds)
+print(f'So you are {kg} kg so better go to gym brooo')
+
+
+
+
+course ='Python for Beginners'
+print(course.find("P"))
+print(course.replace("o", '0'))
