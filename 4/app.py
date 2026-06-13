abcc6ed9deba2f93d525554f4d5def3db799ccc9
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2026-06-13 12:30:25 +0500

    finishing learning part of course

--- 4/app.py
+++ 4/app.py
@@ -1,4 +1,10 @@
 from ecommerce.shipping import calc_shipping
+from pathlib import Path
+
+path = Path()
+
+for file in path.glob("*.py"):
+    print(file)
 
 # ecommerce.shipping.calc_shipping()
 calc_shipping()
