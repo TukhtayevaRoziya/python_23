70fc0a27945fd70dbc504bb33ed74863e39b0402
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2026-06-17 07:24:33 +0500

    working with files: creating folder, adding, writing

--- 6/app.py
+++ 6/app.py
@@ -1,15 +1,51 @@
-def read_file ():
-  file = open("characters.txt",'r')
-  # content=file.read()
-  # print(content)
-  # we need to close the file because of saving storage and preventing to get leaked
-  lines = file.readlines()
-  for line in lines:
-    print(line)
+# -------------------- reading method --------------------
+# def read_file():
+#   file = open('characters.txt', 'r')
+#   content = file.read()
+#   print(content)
+#   file.seek(0)
+#   lines = file.readlines()
+#   for line in lines:
+#     print(line)
     
-  file.close()
+#   file.close()
   
+# read_file()
+
+
+# -------------------- writing method --------------------
+
+# characters = ['Kira', 'L', 'Misa', 'Ryuk', 'Rem']
+
+# def write_file(filename):
+#   file = open(filename, 'w+')
+#   for ch in characters:
+#     file.write(ch+ '\n')
+#   file.seek(0)
+#   content = file.read()
+#   print(content)
+
+# write_file("wcharacter.txt")
+
+# -------------------- adding method --------------------
+# more_characters= ['Harry', 'Hermione', 'Draco', 'Ron', 'Luna', ]
+
+# def adding_to_file(filename):
+#   file = open(filename, 'a+')
+#   for ch in more_characters:
+#     file.write(ch+"\n")
+#   file.seek(0)
+#   content = file.read()
+#   print(content)
   
-  
-read_file()
-    
+# adding_to_file("wcharacter.txt")
+
+# -------------------- creating folder method --------------------
+
+from pathlib import Path
+
+def create_path():
+  script_dir = Path(__file__).parent
+  path = script_dir / 'characters'
+  path.mkdir(parents=True, exist_ok=True)
+create_path()
